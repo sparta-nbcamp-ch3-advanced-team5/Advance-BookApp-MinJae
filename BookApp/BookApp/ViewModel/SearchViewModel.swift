f141fbf52fc068ba91ca4e69e52d2edbeb30b4ee
minjae <qlqlzbto@naver.com>
2025-05-12 02:09:58 +0900

    feat: 데이터 호출 시 ViewModel의 fetchedBooks에 수신

--- BookApp/BookApp/ViewModel/SearchViewModel.swift
+++ BookApp/BookApp/ViewModel/SearchViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+final class SearchViewModel {
+    
+    private let networkManager: NetworkManager
+    private let disposeBag = DisposeBag()
+    private(set) var fetchedBooks = PublishSubject<[Book]>()
+    
+    init(networkManager: NetworkManager = .init()) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchBooks(query: String) {
+        Task {
+            await networkManager.fetch(query: query)
+                .subscribe(onSuccess: {[weak self] (data: BookResponse) in
+                    self?.fetchedBooks.onNext(data.documents)
+                }, onFailure: { error in
+                    print(error.localizedDescription)
+                }).disposed(by: disposeBag)
+        }
+    }
+    
+}
