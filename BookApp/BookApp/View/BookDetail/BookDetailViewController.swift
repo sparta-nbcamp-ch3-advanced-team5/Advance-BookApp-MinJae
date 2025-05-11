0bbaa9b73d93493442213384de715efefbd88d5e
minjae <qlqlzbto@naver.com>
2025-05-11 23:10:20 +0900

    feat: 디렉 수정 및 BookDetailVC 생성

---
+++ BookApp/BookApp/View/BookDetail/BookDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BookDetailViewController.swift
+//  BookApp
+//
+//  Created by MJ Dev on 5/11/25.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
