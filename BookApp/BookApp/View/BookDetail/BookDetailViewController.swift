c8035afd3c6654c23193c5cd543ce08169ec194b
minjae <qlqlzbto@naver.com>
2025-05-12 19:12:56 +0900

    feat: VC에서 FooterView의 버튼 RxCocoa 바인딩 / 생성시 ViewModel생성하고 ViewModel이 데이터를 갖도록 수정

--- BookApp/BookApp/View/BookDetail/BookDetailViewController.swift
+++ BookApp/BookApp/View/BookDetail/BookDetailViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 final class BookDetailViewController: UIViewController {
     
     private var bookDetailView: BookDetailView
+    private let disposeBag = DisposeBag()
+    private let viewModel: BookDetailViewModel
     
     init(book: Book) {
-        self.bookDetailView = BookDetailView(book: book)
+        self.viewModel = BookDetailViewModel(item: book)
+        self.bookDetailView = BookDetailView(book: viewModel.item)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -25,6 +30,7 @@ final class BookDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupUI()
+        bind()
     }
     
     private func setupUI() {
@@ -33,6 +39,25 @@ final class BookDetailViewController: UIViewController {
         bookDetailView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+    }
+    
+    private func bind() {
+        // FooterView의 X버튼 탭 이벤트 바인딩
+        bookDetailView.bookDetailFooterView.cancelButton.rx.tap
+            .observe(on: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe { event in
+                self.dismiss(animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        // FooterView의 담기버튼 탭 이벤트 바인딩
+        bookDetailView.bookDetailFooterView.addButton.rx.tap
+            .withUnretained(self)
+            .subscribe{ event in
+                self.viewModel.saveBook()
+            }
+            .disposed(by: disposeBag)
     }
     
 }
