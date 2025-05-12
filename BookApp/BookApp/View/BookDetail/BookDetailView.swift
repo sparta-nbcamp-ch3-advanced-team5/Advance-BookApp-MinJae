b5ffd775d3a8220f70003d160932d4d1f7922708
minjae <qlqlzbto@naver.com>
2025-05-12 17:15:38 +0900

    style: 책 상세페이지 footerView 높이 변경

--- BookApp/BookApp/View/BookDetail/BookDetailView.swift
+++ BookApp/BookApp/View/BookDetail/BookDetailView.swift
@@ -30,7 +30,7 @@ final class BookDetailView: UIView {
         scrollView.addSubview(bookDetailStackView)
         bookDetailFooterView.snp.makeConstraints {
             $0.leading.bottom.trailing.equalTo(self.safeAreaLayoutGuide)
-            $0.height.equalTo(80)
+            $0.height.equalTo(100)
         }
         scrollView.snp.makeConstraints {
             $0.width.equalToSuperview()
