38d44fcbdc996cc53e882dee5bb520ecf16f656c
minjae <qlqlzbto@naver.com>
2025-05-12 20:36:10 +0900

    feat: 팝업 뷰 구현

--- BookApp/BookApp/View/Search/SearchPopupView.swift
+++ BookApp/BookApp/View/Search/SearchPopupView.swift
@@ -5,4 +5,44 @@
 //  Created by MJ Dev on 5/12/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class SearchPopupView: UIView {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = .gray
+        self.layer.cornerRadius = 12
+        self.clipsToBounds = true
+        self.isHidden = true
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        [label].forEach {
+            self.addSubview($0)
+        }
+        
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+    }
+    
+    func configureLabelText(title: String) {
+        self.label.text = "✅ \(title)책 담기 완료!"
+    }
+}
