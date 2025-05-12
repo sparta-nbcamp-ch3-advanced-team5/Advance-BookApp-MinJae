98596977b5944cd0f5905348d14b165a340c9f9f
minjae <qlqlzbto@naver.com>
2025-05-12 19:23:53 +0900

    feat: ViewModel에서 불러온 데이터 방출 시 적용되도록 VC에서 바인드, 뷰가 나타날 때마다 데이터를 불러오도록 구현

--- BookApp/BookApp/View/MyBook/MyBookViewController.swift
+++ BookApp/BookApp/View/MyBook/MyBookViewController.swift
@@ -7,15 +7,23 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 
 final class MyBookViewController: UIViewController {
 
     private let myBookCollectionView = BookListCollectionView(section: .myBook)
+    private let viewModel = MyBookViewModel()
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupUI()
+        bind()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        viewModel.fetchMyBooks()
     }
     
     private func setupUI() {
@@ -25,5 +33,15 @@ final class MyBookViewController: UIViewController {
             $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview().inset(20)
         }
+    }
+    
+    private func bind() {
+        viewModel.myBooks
+            .observe(on: MainScheduler.instance)
+            .withUnretained(self)
+            .subscribe{ owner, books in
+                owner.myBookCollectionView.apply(at: .myBook, to: books)
+            }
+            .disposed(by: disposeBag)
     }
 }
