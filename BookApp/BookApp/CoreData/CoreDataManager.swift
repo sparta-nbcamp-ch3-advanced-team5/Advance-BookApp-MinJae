d47ea279964531820e4e61645d476324ea68b460
minjae <qlqlzbto@naver.com>
2025-05-15 16:57:29 +0900

    feat: 코어데이터매니저의 Read, Create 메서드를 엔티티 열거형 파라미터에 따른 다른 엔티티모델을 불러오도록 수정

--- BookApp/BookApp/CoreData/CoreDataManager.swift
+++ BookApp/BookApp/CoreData/CoreDataManager.swift
@@ -14,12 +14,21 @@ final class CoreDataManager {
     private lazy var context = appDelegate.persistentContainer.viewContext
     
     // CREATE
-    func create(to book: Book) {
-        guard let entity = NSEntityDescription.entity(forEntityName: CoreDataEntity.MyBook, in: context) else { return }
-        let newBook = NSManagedObject(entity: entity, insertInto: context)
-        newBook.setValue(book.title, forKey: MyBookKey.title)
-        newBook.setValue(book.authors.joined(separator: ", "), forKey: MyBookKey.author)
-        newBook.setValue(book.price, forKey: MyBookKey.price)
+    func create<T: Decodable>(for entityModel: CoreDataEntity,
+                to item: T) {
+        guard let entity = NSEntityDescription.entity(forEntityName: entityModel.entityName, in: context) else { return }
+        let newItem = NSManagedObject(entity: entity, insertInto: context)
+        let mirror = Mirror(reflecting: item)
+        let keys = entityModel.object.keys
+        mirror.children.forEach { child in
+            if let label = child.label, keys.contains(label) {
+                if let authors = child.value as? [String] {
+                    newItem.setValue(authors.joined(separator: ", "), forKey: label)
+                } else {
+                    newItem.setValue(child.value, forKey: label)
+                }
+            }
+        }
         
         do {
             try context.save()
@@ -29,26 +38,49 @@ final class CoreDataManager {
     }
     
     // READ
-    func read() -> [Book] {
+    func read(for entityModel: CoreDataEntity) -> [Book] {
+        var objectType: NSManagedObject.Type?
+        
+        if case .myBook = entityModel {
+            objectType = entityModel.myBookObject.type
+        }
+        if case .recentBook = entityModel {
+            objectType = entityModel.recentBookObject.type
+        }
+        
+        guard let type = objectType else {
+            print("Type Casting Error")
+            return []
+        }
+        
+        let keys = entityModel.object.keys
+        
         do {
-            let books = try context.fetch(MyBook.fetchRequest())
+            let books = try context.fetch(type.fetchRequest())
             var result = [Book]()
-            for element in books {
-                guard let title = element.title,
-                      let author = element.author
-                else {
-                    continue
+            if let datas = books as? [NSManagedObject] {
+                for element in datas {
+                    var data = [String: Any]()
+                    keys.map{ data[$0] = element.value(forKey: $0) }
+                    if let title = data["title"] as? String,
+                       let authors = data["authors"] as? String,
+                       let price = data["price"] as? Int
+                    {
+                        result.append(
+                            Book(title: title,
+                                 authors: authors.components(separatedBy: ", "),
+                                 price: price,
+                                 description: data["descriptionString"] as? String ?? nil,
+                                 imageURL: data["imageURLString"] as? String ?? nil,
+                                 isRecent: false
+                                )
+                        )
+                    }
                 }
-                result.append(Book(title: title,
-                                   authors: author.components(separatedBy: " ,"),
-                                   price: Int(element.price),
-                                   description: nil,
-                                   imageURL: nil)
-                )
             }
             return result
         } catch {
-            print("CoreDataManager Read error")
+            print("READ ERROR")
             return []
         }
     }
